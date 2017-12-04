5dcf9e00e141b54cae1f2af1248b6d39696ad6d8
Kenil Vora <kenilvora97@gmail.com>
2017-12-03 18:16:00 -0800

    Alexa with Airplay working

---
+++ samples/companionService/config.js
@@ -0,0 +1,31 @@
+/**
+ * @module
+ * This module defines the settings that need to be configured for a new
+ * environment.
+ * The clientId and clientSecret are provided when you create
+ * a new security profile in Login with Amazon.  
+ * 
+ * You will also need to specify
+ * the redirect url under allowed settings as the return url that LWA
+ * will call back to with the authorization code.  The authresponse endpoint
+ * is setup in app.js, and should not be changed.  
+ * 
+ * lwaRedirectHost and lwaApiHost are setup for login with Amazon, and you should
+ * not need to modify those elements.
+ */
+var config = {
+    clientId: "amzn1.application-oa2-client.8611504017a64868a8996690cb3d54c3",
+    clientSecret: "3f8fca17f1aa241dd9976072066fdf6e8e4788cc61b9fb0e72ad42ec75af2b91",
+    redirectUrl: 'https://localhost:3000/authresponse',
+    lwaRedirectHost: "amazon.com",
+    lwaApiHost: "api.amazon.com",
+    validateCertChain: true,
+    sslKey: "/home/pi/Desktop/alexa-avs-sample-app/samples/javaclient/certs/server/node.key",
+    sslCert: "/home/pi/Desktop/alexa-avs-sample-app/samples/javaclient/certs/server/node.crt",
+    sslCaCert: "/home/pi/Desktop/alexa-avs-sample-app/samples/javaclient/certs/ca/ca.crt",
+    products: {
+        "my_pi": ["123456789"],
+    },
+};
+
+module.exports = config;
